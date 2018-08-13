77094c776b02ade1306425090745efa913698b7c
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-08-13 14:21:01 -0700

    style fixes with prettier

--- backend/models/outfitModel.js
+++ backend/models/outfitModel.js
@@ -1,28 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const OutfitSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    tags: [{
-        type: String,
-    }],
-    worn: [{
-        type: Date,
-    }],
-    top: [{ // changing this to an array to account for layers
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Item',
-    }],
-    bottom: [{ // changing this to an array to account for layers
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Item',
-    }],
-    shoes: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Item',
-    },
+  name: {
+    type: String,
+    required: true
+  },
+  tags: [
+    {
+      type: String
+    }
+  ],
+  worn: [
+    {
+      type: Date
+    }
+  ],
+  top: [
+    {
+      // changing this to an array to account for layers
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item"
+    }
+  ],
+  bottom: [
+    {
+      // changing this to an array to account for layers
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Item"
+    }
+  ],
+  shoes: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Item"
+  }
 });
 
-module.exports = mongoose.model('Outfit', OutfitSchema);
+module.exports = mongoose.model("Outfit", OutfitSchema);
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
