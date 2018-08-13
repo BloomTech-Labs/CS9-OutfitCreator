256e8bc727ec1c1f154f4bfe0d3496eec30abef9
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-08-13 10:01:25 -0700

    added to mongoose schemas

--- backend/models/itemModel.js
+++ backend/models/itemModel.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
-    
+    type: {
+        type: String,
+        required: true,
+        enum: ['top', 'bottom', 'shoes'],
+    },
+    tags: [{
+        type: String,
+    }],
 });
 
 module.exports = mongoose.model('Item', ItemSchema);
--- backend/models/outfitModel.js
+++ backend/models/outfitModel.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose');
 
 const outfitSchema = new mongoose.Schema({
-    
+    name: {
+        type: String,
+        required: true,
+    },
+    tags: [{
+        type: String,
+    }],
+    worn: [{
+        type: Date,
+    }],
+    top: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item',
+    },
+    bottom: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item',
+    },
+    shoes: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Item',
+    },
 });
 
 module.exports = mongoose.model('Outfit', OutfitSchema);
