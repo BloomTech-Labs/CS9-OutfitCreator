c577dbad2629acd3efebf7a41eadf3053b6af04a
Devon Bacon <devon.bacon@gmail.com>
2018-08-14 16:24:32 -0700

    Put in filler renders for each components so that data is returned

---
+++ frontend/outfitcreator/src/Components/Archive.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Archive extends Component {
+  render() {
+    return (
+      <div className='Archive'>
+        Archive
+      </div>
+    );
+  }
+}
+
+export default Archive;
---
+++ frontend/outfitcreator/src/Components/Billings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Billings extends Component {
+  render() {
+    return (
+      <div className='Billings'>
+        Billings
+      </div>
+    );
+  }
+}
+
+export default Billings;
