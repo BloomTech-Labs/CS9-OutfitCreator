273ae4b3498e6618680688d6f85f8a86e6ca689b
Justin M <jekm321@gmail.com>
2018-08-14 14:09:21 -0400

    cleaned up App.js to remove the default header as well as the blank endpt of /Signup, moved locaiton of landing.js and updated the import path for that

--- frontend/outfitcreator/src/App.js
+++ frontend/outfitcreator/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Landing from './Components/Landing';
+import Landing from './Components/Landing_Page/Landing';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
@@ -8,19 +7,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
 
         <Switch>
           <Route exact path='/' component={Landing} />
           <Route exact path='/SignUp' />
         </Switch>
-
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
