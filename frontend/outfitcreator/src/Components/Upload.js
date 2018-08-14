67a1658d1785e9da778e2005ab3abf07328b5b35
Devon Bacon <devon.bacon@gmail.com>
2018-08-14 16:25:31 -0700

    Put in filler renders for each components so that data is returned

---
+++ frontend/outfitcreator/src/Components/Upload.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Upload extends Component {
+  render() {
+    return (
+      <div className="Upload">
+        Upload
+      </div>
+    );
+  }
+}
+
+export default Upload;
+
