4b9aa619e8f5299bccbeac7c16ac13d88821ae3f
Julian Alexander <julian.pineyro@gmail.com>
2018-08-14 20:55:35 +0100

    OAuth and Mongo user creation successful. Adding Cookie handlers

--- backend/config/passport-setup.js
+++ backend/config/passport-setup.js
@@ -3,6 +3,16 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
 const User = require('../models/userModel');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);        
+    });
+});
+
 passport.use(
     new GoogleStrategy({
         // options for google strategy
@@ -11,7 +21,21 @@ passport.use(
         clientSecret: keys.google.clientSecret
     }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
-        console.log('Callback firing');
-        console.log(profile);
+        // console.log('Callback firing');
+        // console.log(profile);
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser) {
+                console.log('user is: ', currentUser);
+                done(null, currentUser);
+            } else {
+                new User({
+                    googleId: profile.id,
+                    username: profile.displayName
+                }).save().then((newUser) => {
+                    console.log('new user created: ', newUser);
+                    done(null, newUser);
+                });
+            }
+        });
     })
 );
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   username: {
     type: String,
     unique: true,
     required: true
   },
   googleId: String,
-  password: {
-    type: String,
-    required: true
-  }
+  // password: {
+  //   type: String,
+  //   required: true
+  // }
 });
 
- const User = mongoose.model("User", UserSchema);
- module.exports = User;
+// const GuserSchema = new Schema ({
+//   username: String,
+//   googleId: String
+// });
+
+// const Guser = mongoose.model('guser', GuserSchema);
+// module.exports = Guser;
+
+module.exports = mongoose.model("User", UserSchema);
