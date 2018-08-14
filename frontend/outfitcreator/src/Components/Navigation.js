d6879542adf8ebdd93722a813d8bfeec47bd3e21
Justin M <jekm321@gmail.com>
2018-08-14 19:31:12 -0400

    fixed this.props.location using withRouter

--- frontend/outfitcreator/src/Components/Navigation.js
+++ frontend/outfitcreator/src/Components/Navigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import { withRouter } from 'react-router';
 import './Navigation.css';
 
 class Navigation extends Component {
@@ -7,7 +8,7 @@ class Navigation extends Component {
     return (
       <div className='Navigation'>
         <div className='BreadCrumbs'>
-          {/* <a href='/Landing' className='Home'>Home</a> > {this.props.location.pathname.slice(1)} */}
+          <a href='/Landing' className='Home'>Home</a> > {this.props.location.pathname.slice(1)}
         </div>
         <Nav vertical className='SideNav'>
           <NavItem>
@@ -32,4 +33,5 @@ class Navigation extends Component {
   }
 }
 
-export default Navigation;
+
+export default withRouter(Navigation);
