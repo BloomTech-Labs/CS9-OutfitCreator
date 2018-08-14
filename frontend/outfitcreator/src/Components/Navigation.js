7f237bf8fea4a69bfebaeaea49b8740228033829
Devon Bacon <devon.bacon@gmail.com>
2018-08-14 12:45:26 -0700

    Navigation.js - added bread crumbs

--- frontend/outfitcreator/src/Components/Navigation.js
+++ frontend/outfitcreator/src/Components/Navigation.js
@@ -5,25 +5,29 @@ import './Navigation.css';
 class Navigation extends Component {
   render() {
     return (
-      <div className="Navigation">
-        <Nav vertical className="SideNav">
+      <div className='Navigation'>
+          <div className='BreadCrumbs'>
+            Home > {this.props.location.pathname.slice(1)}
+          </div>
+        <Nav vertical className='SideNav'>
+          
           <NavItem>
-            <NavLink href="/Create" className="Create">Create</NavLink>
+            <NavLink href='/Create' className='Create'>Create</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/Upload" className="Upload">Upload</NavLink>
+            <NavLink href='/Upload' className='Upload'>Upload</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/Archive" className="Archive">Archive</NavLink>
+            <NavLink href='/Archive' className='Archive'>Archive</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/Settings" className="Settings">Settings</NavLink>
+            <NavLink href='/Settings' className='Settings'>Settings</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/Billing" className="Billing">Billing</NavLink>
+            <NavLink href='/Billing' className='Billing'>Billing</NavLink>
           </NavItem>
         </Nav>
-        <NavLink href="/Landing" className="SignOut">Sign Out</NavLink>
+        <NavLink href='/Landing' className='SignOut'>Sign Out</NavLink>
       </div>
     );
   }
