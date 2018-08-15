92d3a2e564c1abab1b134bdd20e3b5ec4e5ab7aa
Devon Bacon <devon.bacon@gmail.com>
2018-08-15 14:21:10 -0700

    TagSearch - built basics of tag system

--- frontend/outfitcreator/src/Components/Upload_Page/TagSearch.js
+++ frontend/outfitcreator/src/Components/Upload_Page/TagSearch.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { Input, InputGroup, InputGroupAddon } from 'reactstrap';
+import { Input, InputGroup, InputGroupAddon, Form, FormGroup, Label } from 'reactstrap';
 import './TagSearch.css';
 import SearchIcon from '../../search.png';
 
 class TagSearch extends Component {
   state = {
-    query: 'Testing',
+    query: '',
     results: [],
-    tags: ['some', 'cool', 'tags', 'go', 'here']
+    tags: ['some', 'cool', 'tags', 'go', 'here', 'testing', 'wrap']
+  }
+
+  addTag = () => {
+    
+  }
+
+  removeTag = () => {
+
   }
 
   getTags = () => {
@@ -21,23 +29,41 @@ class TagSearch extends Component {
   render () {
     return (
       <div>
-        <InputGroup className="TagSearch">
-          <InputGroupAddon className="SearchIcon" addonType="prepend"><img src={SearchIcon} alt="Magnifying Glass" /></InputGroupAddon>
-          <Input className="Search" placeholder="Search" value={this.state.query} onChange={(e) => this.handleInputChange(e)} />
+        <InputGroup className='TagSearch'>
+          <InputGroupAddon className='SearchIcon' addonType='prepend'><img src={SearchIcon} alt='Magnifying Glass' /></InputGroupAddon>
+          <Input className='Search' placeholder='Search' value={this.state.query} onChange={(e) => this.handleInputChange(e)} />
         </InputGroup>
 
-        <div className="TagView">
-          {/* {this.state.tags.forEach(tag => (
-            <div className={'Tag-' + tag} key={this.props.tags.indexOf(tag)}>
-              {tag}
-            </div>
-          ))} */}
-          {this.state.tags.forEach(tag => (
-            <div className={'Tag-' + tag} key={this.state.tags.indexOf(tag)}>
-              {tag}
+        {/* Display tag text for each tag in state */}
+        <div className='TagView'>
+          {this.state.tags.map(tag => (
+            <div className={'Tag ' + tag} key={this.state.tags.indexOf(tag)}>
+              <span className='DeleteTag'>x</span>{tag}
             </div>
           ))}
         </div>
+
+        <Label>Pair With:</Label>
+        <Form className="PairWith">
+          <FormGroup check>
+            <Label check>
+              <Input type="checkbox" />
+              Bottoms
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="checkbox" />
+              Tops
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="checkbox" />
+              Shoes
+            </Label>
+          </FormGroup>
+        </Form>
       </div>
     );
   }
