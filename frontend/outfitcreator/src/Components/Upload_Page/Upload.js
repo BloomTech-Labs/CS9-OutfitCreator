aae5786ed1ce844359093e26bee1e7821505c0ac
Devon Bacon <devon.bacon@gmail.com>
2018-08-16 10:36:54 -0700

    Upload.js - improved consistency

--- frontend/outfitcreator/src/Components/Upload_Page/Upload.js
+++ frontend/outfitcreator/src/Components/Upload_Page/Upload.js
@@ -4,20 +4,22 @@ import TagSearch from './TagSearch';
 import './Upload.css';
 
 class Upload extends Component {
+
+
   render () {
     return (
-      <div className="UploadPage">
+      <div className='UploadPage'>
         <div className='UploadColumns'>
-          <div className="UploadLeft">
-            <CardImg className="UploadImage" 
-              src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-              alt="Upload Image Thumbnail" />
-            <Button className="UploadButton">Upload</Button>
+          <div className='UploadLeft'>
+            <CardImg className='UploadImage' 
+              src='https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180'
+              alt='Upload Image Thumbnail' />
+            <Button className='UploadButton'>Upload</Button>
             <FormGroup>
-              <Input id="ClothingName" type="text" name="name" placeholder="Clothing Name" />
+              <Input id='ClothingName' type='text' placeholder='Clothing Name' autoComplete='off' />
             </FormGroup>
             <FormGroup>
-              <Input id="SelectType" type="select" name="select">
+              <Input id='SelectType' type='select' name='select'>
                 <option>Top</option>
                 <option>Bottom</option>
                 <option>Shoes</option>
@@ -25,13 +27,13 @@ class Upload extends Component {
             </FormGroup>
           </div>
 
-          <div className="UploadRight">
+          <div className='UploadRight'>
             <TagSearch />
             
           </div>
         </div>
 
-        <Button className="SaveButton">Save</Button>
+        <Button className='SaveButton'>Save</Button>
       </div>
     );
   }
