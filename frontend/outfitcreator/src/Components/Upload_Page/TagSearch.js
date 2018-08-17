9542a6d4d5d583add8f01e6f9be917a1c02cfe30
Devon Bacon <devon.bacon@gmail.com>
2018-08-16 19:17:35 -0700

    matched style conventions of other front end components like Archive.js

--- frontend/outfitcreator/src/Components/Upload_Page/TagSearch.js
+++ frontend/outfitcreator/src/Components/Upload_Page/TagSearch.js
@@ -1,80 +1,60 @@
 import React, { Component } from 'react';
-import { Input, InputGroup, InputGroupAddon, Form, FormGroup, Label } from 'reactstrap';
+import { Input, InputGroup, InputGroupAddon,  } from 'reactstrap';
 import './TagSearch.css';
 import SearchIcon from '../../search.png';
 
 class TagSearch extends Component {
-  constructor(props) {
-    super();
-  }
+    constructor(props) {
+        super();
 
-  addTag = (e) => {
-    const newState = { ...this.props.state };
-    if(e.key === 'Enter' && !newState.tags.includes(newState.query)){
-      newState.tags.push(newState.query);
-      newState.query = '';
-      this.props.updateState(newState);
+        console.log(props);
     }
-  }
 
-  removeTag = (e) => {
-    const newState = { ...this.props.state };
-    newState.tags = newState.tags.filter(tag => tag !== e.target.nextElementSibling.innerHTML);
-    this.props.updateState(newState);
-  }
+    addTag = (e) => {
+        const newState = { ...this.props.state };
+        if(e.key === 'Enter' && !newState.tags.includes(newState.query)){
+            newState.tags.push(newState.query);
+            newState.query = '';
+            this.props.updateState(newState);
+        }
+    }
 
-  getTags = () => {
-    // Axios call
-  }
+    removeTag = (e) => {
+        const newState = { ...this.props.state };
+        newState.tags = newState.tags.filter(tag => tag !== e.target.nextElementSibling.innerHTML);
+        this.props.updateState(newState);
+    }
 
-  handleInputChange = (e) => {
-    const newState = { ...this.props.state };
-    newState.query = e.target.value;
-    this.props.updateState(newState);
-  }
+    getTags = () => {
+      // Axios call
+    }
 
-  render () {
-    return (
-      <div>
-        <InputGroup className='TagSearch'>
-          <InputGroupAddon className='SearchIcon' addonType='prepend'><img src={SearchIcon} alt='Magnifying Glass' /></InputGroupAddon>
-          <Input className='Search' type='text' placeholder='Search' value={this.props.state.query} onChange={this.handleInputChange} onKeyPress={this.addTag} />
-        </InputGroup>
+    handleInputChange = (e) => {
+        const newState = { ...this.props.state };
+        newState.query = e.target.value;
+        this.props.updateState(newState);
+    }
 
-        {/* Display tag text for each tag in state */}
-        <div className='TagView'>
-          {this.props.state.tags.map(tag => (
-            <div className={'Tag ' + tag} key={this.props.state.tags.indexOf(tag)}>
-              <span className='DeleteTag' onClick={this.removeTag}>x</span>
-              <span>{tag}</span>
+    render () {
+        return (
+          <div className='container--tagSearch'>
+            <InputGroup>
+                <InputGroupAddon className='tagSearch--icon' addonType='prepend'><img src={SearchIcon} alt='Magnifying Glass' /></InputGroupAddon>
+                <Input className='tagSearch--input' type='text' placeholder='Search' value={this.props.state.query} onChange={this.handleInputChange} onKeyPress={this.addTag} />
+            </InputGroup>
+
+            {/* Display tag text for each tag in state */}
+            <div className='tagSearch--view'>
+                {this.props.state.tags.map(tag => (
+                    <div className={'tagSearch--tag ' + tag} key={this.props.state.tags.indexOf(tag)}>
+                        <span className='tagSearch--delete' onClick={this.removeTag}>x</span>
+                        <span>{tag}</span>
+                    </div>
+                ))}
             </div>
-          ))}
-        </div>
-
-        <Label>Pair With:</Label>
-        <Form className='PairWith'>
-          <FormGroup check>
-            <Label check>
-              <Input type='checkbox' id='TopsCheckbox'/>
-              Tops
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type='checkbox' id='BottomsCheckbox' />
-              Bottoms
-            </Label>
-          </FormGroup>
-          <FormGroup check>
-            <Label check>
-              <Input type='checkbox' id='ShoesCheckbox' />
-              Shoes
-            </Label>
-          </FormGroup>
-        </Form>
-      </div>
-    );
-  }
+          </div>
+        );
+    }
 }
 
 export default TagSearch;
--- frontend/outfitcreator/src/Components/Upload_Page/Upload.js
+++ frontend/outfitcreator/src/Components/Upload_Page/Upload.js
@@ -1,95 +1,156 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { CardImg, Button, FormGroup, Input } from 'reactstrap';
+import { CardImg, Button, Form, FormGroup, Input, Label } from 'reactstrap';
 import TagSearch from './TagSearch';
 import './Upload.css';
 
+
+// TO DO
+// Refactor so that every input has its respective name
+// Create universal event listener for all fields that set state
+// Make axios calls with state
+
+
 class Upload extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      image: '',
-      query: '',
-      results: [],
-      tags: ['some', 'cool', 'tags', 'go', 'here', 'testing', 'wrap']
+      constructor(props) {
+        super(props);
+        this.state = { // image, name, type, search, tags, pairWith
+            image: '',
+            pairWith: [],
+            name: '',
+            search: '',
+            tags: ['some', 'cool', 'tags', 'go', 'here', 'testing', 'wrap'],
+            type: ''
+        }
     }
-  }
 
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log(this.state.image);
-    // this.setState({ image: URL.createObjectURL(event.target.files[0])});
-    // const formData = new FormData({
-    //   name: 'myImage',
-    //   image: this.state.image,
-    //   imageName: 'image name here'
-    // });
-    // formData.name = 'myImage';
-    // formData.image = this.state.image;
-    // formData.imageName = 'image name';
-    // console.log(formData);
-    axios.post('http://localhost:5000/item', {
-      // user: 'jekm321',
-      name: 'jekms\'s test',
-      image: this.state.image,
-      type: 'top',
-    })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.image);
+        // this.setState({ image: URL.createObjectURL(event.target.files[0])});
+        // const formData = new FormData({
+        //   name: 'myImage',
+        //   image: this.state.image,
+        //   imageName: 'image name here'
+        // });
+        // formData.name = 'myImage';
+        // formData.image = this.state.image;
+        // formData.imageName = 'image name';
+        // console.log(formData);
+        axios.post('http://localhost:5000/item', {
+            // user: 'jekm321',
+            name: 'jekms\'s test',
+            image: this.state.image,
+            type: 'top',
+        })
+          .then(response => {
+              console.log(response);
+          })
+          .catch(err => {
+              console.log(err);
+          });
+      }
 
-  fileChanged = event => {
-    console.log(event.target.files);
-    // this.setState({ image: event.target.files[0] });
-    this.setState({ image: URL.createObjectURL(event.target.files[0]) });
-  }
+    fileChanged = event => {
+        console.log(event.target.files);
+        // this.setState({ image: event.target.files[0] });
+        this.setState({ image: URL.createObjectURL(event.target.files[0]) });
+    }
 
-  updateState = (state) => {
-    this.setState(state);
-  }
 
-  render() {
-    return (
-      <div className='UploadPage'>
-        <div className='UploadColumns'>
-          <div className="UploadLeft">
-            {this.state.image ?
-              <CardImg className="UploadImage"
-                src={this.state.image}
-                alt="Upload Image Thumbnail" /> :
-              <CardImg className="UploadImage"
-                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
-                alt="Upload Image Thumbnail" />}
-            <Button className="UploadButton">Upload</Button>
-            <FormGroup>
-              <Input type='file' name='clothing' id='image--upload' onChange={this.fileChanged} />
-              <br />
-              <Input type="text" name="name" id="ClothingName" placeholder="Clothing Name" autoComplete='off' />
-            </FormGroup>
-            <FormGroup>
-              <Input id='SelectType' type='select' name='select'>
-                <option>Top</option>
-                <option>Bottom</option>
-                <option>Shoes</option>
-              </Input>
-            </FormGroup>
-          </div>
+    handleInputChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
 
-          <div className="UploadRight">
-            <TagSearch state={this.state} updateState={this.updateState} />
+    updateState = state => {
+        this.setState(state);
+    }
 
-          </div>
+    render() {
+      return (
+        <div className='container--upload'>
+            <div className='upload--columns'>
+                <div className="column--left">
+                    {this.state.image ?
+                        <CardImg 
+                            className="upload--image"
+                            src={this.state.image}
+                            alt="Upload Image Thumbnail" 
+                        /> :
+                        <CardImg 
+                            className="upload--image"
+                            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                            alt="Upload Image Thumbnail" 
+                        />
+                    }
+                    <Button className="upload--button">Upload</Button>
+                    <FormGroup>
+                        <Input 
+                            type='file' 
+                            name='clothing' 
+                            id='image--upload' 
+                            onChange={this.fileChanged} 
+                        />
+                        <br />
+                        <Input 
+                            type="text" 
+                            name="name" 
+                            className="upload--name" 
+                            placeholder="Clothing Name" 
+                            autoComplete='off' 
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Input className='upload--select' type='select' name='select'>
+                            <option>Top</option>
+                            <option>Bottom</option>
+                            <option>Shoes</option>
+                        </Input>
+                    </FormGroup>
+                </div>
+
+                <div className="column--right">
+                    <TagSearch 
+                        state={this.state} 
+                        updateState={this.updateState} 
+                    />
+                    <Label>Pair With:</Label>
+                    <Form className='upload--pairWith'>
+                        <FormGroup check>
+                            <Label check>
+                              <Input 
+                                  type='checkbox' 
+                                  id='upload--topsCheckbox'
+                              />
+                              Tops
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input 
+                                    type='checkbox' 
+                                    id='upload--bottomsCheckbox' 
+                                />
+                                Bottoms
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check>
+                            <Label check>
+                                <Input 
+                                    type='checkbox' 
+                                    id='upload--shoesCheckbox' 
+                                />
+                                Shoes
+                            </Label>
+                        </FormGroup>
+                    </Form>
+                </div>
+            </div>
+
+            <Button className="upload--save" onClick={this.handleSubmit}>Save</Button>
         </div>
-
-        <Button className="SaveButton" onClick={this.handleSubmit}>Save</Button>
-
-      </div>
-    );
-  }
+      );
+    }
 }
 
 export default Upload;
