5e868455592d5f66781d27903916228da2aac3d5
Devon Bacon <devon.bacon@gmail.com>
2018-08-17 16:23:17 -0700

    userModel.js - added bcrypt hashing for later

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -19,5 +20,20 @@ const UserSchema = new mongoose.Schema({
         default: false,
     }
 });
+
+// UserSchema.pre('create', function(next) {
+//     bcrypt.hash(this.password, 11, (err, hash) => {
+//         if (err) return next(err);
+//         this.password = hash;
+//         next();
+//     });
+// })
+
+// UserSchema.methods.checkPassword(plainTextPW, this.password, (err, isMatch) => {
+//     bcrypt.compare(passwordGuess, this.password, (err, isMatch) => {
+//         if (err) return cb(err);
+//         cb(null, isMatch);
+//     });
+// })
 
 module.exports = mongoose.model("User", UserSchema);
