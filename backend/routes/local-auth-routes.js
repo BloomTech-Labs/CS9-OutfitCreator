1fba69770f526367b2b0e69bbe59a4a58eea7da5
Devon Bacon <devon.bacon@gmail.com>
2018-08-17 16:22:55 -0700

    local-auth-routes.js - created strategy for local auth

---
+++ backend/routes/local-auth-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) return done(err); 
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
+// Routes
+
+module.exports = router;
