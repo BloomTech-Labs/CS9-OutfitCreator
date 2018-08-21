bd298f5a14dedc195d82113cb08ef60cd669f6a3
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-08-20 23:01:27 -0700

    updated new API documentation

--- backend/routes/item-routes.js
+++ backend/routes/item-routes.js
@@ -1,45 +1,47 @@
-const multer = require('multer');
-const fs = require('fs');
-const cloudinary = require('cloudinary');
+const multer = require("multer");
+const fs = require("fs");
+const cloudinary = require("cloudinary");
 
 const Item = require("../models/itemModel");
 
 const router = require("express").Router();
 
-
-require('dotenv').config();
+require("dotenv").config();
 cloudinary.config({
-  cloud_name: 'cloudtesting',
-  api_key: '465735684648442',
-  api_secret: 'HVxIWBW7bQaBHJygz_qiprAfwok',
+  cloud_name: "cloudtesting",
+  api_key: "465735684648442",
+  api_secret: "HVxIWBW7bQaBHJygz_qiprAfwok"
 });
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, './uploads/');
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.originalname);
-    }
-  })
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
 
 const jekmsUpload = multer({ storage: storage });
 // const upload = multer({ dest: './uploads' });
 // Add a new item to the database
-router.post("/", jekmsUpload.single('image'), (req, res) => {
+router.post("/", jekmsUpload.single("image"), (req, res) => {
   // console.log('req.body: ' + req.body);
   const { user, name, type, tags } = req.body;
   const { originalname } = req.file;
   // console.log(req.file);
-  cloudinary.uploader.upload(`./uploads/${originalname}`, (result) => {
+  cloudinary.uploader.upload(`./uploads/${originalname}`, result => {
     fs.unlinkSync(`./uploads/${originalname}`);
-    const {width, height, url} = result;
-    let cropWidth = width, cropHeight = height;
-    while(cropWidth >= 300 || cropHeight >= 200) {
-      cropWidth *= .9, cropHeight *= .9;
+    const { width, height, url } = result;
+    let cropWidth = width,
+      cropHeight = height;
+    while (cropWidth >= 300 || cropHeight >= 200) {
+      (cropWidth *= 0.9), (cropHeight *= 0.9);
     }
-    const crop = `/upload/w_${cropWidth.toFixed(0)},h_${cropHeight.toFixed(0)}/`;
-    const [partOne, partTwo] = url.split('/upload/');
+    const crop = `/upload/w_${cropWidth.toFixed(0)},h_${cropHeight.toFixed(
+      0
+    )}/`;
+    const [partOne, partTwo] = url.split("/upload/");
     const image = partOne + crop + partTwo;
     Item.create({ user, name, image, type, tags })
       .then(item => {
@@ -104,40 +106,39 @@ router.post("/tags/:id", (req, res) => {
 
 // Get items by type for a user
 router.get("/:user/:type", (req, res) => {
-    const { user, type } = req.params;
-    Item.find({
-      type,
-      user
+  const { user, type } = req.params;
+  Item.find({
+    type,
+    user
+  })
+    .populate()
+    .then(items => {
+      res.status(200).json(items);
     })
-      .populate()
-      .then(items => {
-        res.status(200).json(items);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ message: "Items could not be retreived at this time." });
-      });
-  });
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "Items could not be retreived at this time." });
+    });
+});
 
-  // Get all of a user's items with a certain tag
+// Get all of a user's items with a certain tag
 router.get("/search/:user/:tag", (req, res) => {
-    const { tag, user } = req.params;
-    Item.find({
-      tags: tag,
-      user: user
+  const { tag, user } = req.params;
+  Item.find({
+    tags: tag,
+    user: user
+  })
+    .populate()
+    .then(items => {
+      res.status(200).json(items);
     })
-      .populate()
-      .then(items => {
-        res.status(200).json(items);
-      })
-      .catch(err => {
-        res.send({ error: err.message });
-      });
-  });
+    .catch(err => {
+      res.send({ error: err.message });
+    });
+});
 
-  module.exports = router;
-
+module.exports = router;
 
 // Get items by type
 // Should probably be deprecated, keeping the code just in case
@@ -154,4 +155,3 @@ router.get("/search/:user/:tag", (req, res) => {
 //         res.status(500).json({ message: 'Items could not be retreived at this time.' })
 //       });
 //   });
-
--- backend/routes/outfit-routes.js
+++ backend/routes/outfit-routes.js
@@ -4,50 +4,50 @@ const router = require("express").Router();
 
 // Add a new outfit to the database
 router.post("/", (req, res) => {
-    const { user, name, tags, worn, top, bottom, shoes } = req.body;
-    Outfit.create({ user, name, tags, worn, top, bottom, shoes })
-      .then(outfit => {
-        res.status(201).json(outfit);
-      })
-      .catch(err => {
-        res.send(500).json({ error: err.message });
-      });
-  });
+  const { user, name, tags, worn, top, bottom, shoes } = req.body;
+  Outfit.create({ user, name, tags, worn, top, bottom, shoes })
+    .then(outfit => {
+      res.status(201).json(outfit);
+    })
+    .catch(err => {
+      res.send(500).json({ error: err.message });
+    });
+});
 
 // Get all outfits for a user
 router.get("/:user", (req, res) => {
-    const user = req.params.user;
-    Outfit.find({
-      user
+  const user = req.params.user;
+  Outfit.find({
+    user
+  })
+    .populate()
+    .then(outfits => {
+      res.status(200).json(outfits);
     })
-      .populate()
-      .then(outfits => {
-        res.status(200).json(outfits);
-      })
-      .catch(err => {
-        res.send({ error: err.message });
-      });
-  });
-  
-  // Get a specific outfit by ID
+    .catch(err => {
+      res.send({ error: err.message });
+    });
+});
+
+// Get a specific outfit by ID
 router.get("/:id", (req, res) => {
-    const id = req.params.id;
-    Outfit.findById(id)
-      .then(outfit => {
-        res.status(200).json(outfit);
-      })
-      .catch(err => {
-        res.send({ error: err.message });
-      });
-  });
+  const id = req.params.id;
+  Outfit.findById(id)
+    .then(outfit => {
+      res.status(200).json(outfit);
+    })
+    .catch(err => {
+      res.send({ error: err.message });
+    });
+});
 
-  // Delete a specific outfit
-  router.delete("/:id", (req, res) => {
-    Outfit.findByIdAndRemove(req.params.id)
-      .then(res.status(200).json(`successfully deleted outfit ${req.params.id}`))
-      .catch(err => {
-        res.send(500).json({ error: err.message });
-      });
-  });
+// Delete a specific outfit
+router.delete("/:id", (req, res) => {
+  Outfit.findByIdAndRemove(req.params.id)
+    .then(res.status(200).json(`successfully deleted outfit ${req.params.id}`))
+    .catch(err => {
+      res.send(500).json({ error: err.message });
+    });
+});
 
-  module.exports = router;
+module.exports = router;
