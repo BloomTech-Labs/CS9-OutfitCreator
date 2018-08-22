53442665156fadaf9b72e97d70b29f0bf6894331
Devon Bacon <devon.bacon@gmail.com>
2018-08-22 11:24:22 -0700

    config.js - set up config file to change URLs based on node environment

---
+++ frontend/outfitcreator/src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ROOT_URL: process.env.NODE_ENV === 'production' ?
+  { 
+    WEB: 'https://lambda-outfit-creator.herokuapp.com/',
+    API: 'https://lambda-outfit-creator-api.herokuapp.com/'
+  } : {
+    WEB: 'http://localhost:3000',
+    API: 'http://localhost:5000'
+  }
+}
