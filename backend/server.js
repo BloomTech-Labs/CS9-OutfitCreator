fc2684f9b5bd296447a7ed2774c8c34db6d12fe9
Devon Bacon <devon.bacon@gmail.com>
2018-08-22 11:47:04 -0700

    server.js - commented out HTTPS set up until SSL is active on heroku

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const helmet = require("helmet");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const port = process.env.PORT || 5001; // HTTPS: changed port to use https on 5000
+const port = process.env.PORT || 5000; // HTTPS: changed port to use https on 5000
 const User = require("./models/userModel");
 
 require("dotenv").config();
@@ -20,15 +20,15 @@ const userRoutes = require("./routes/user-routes");
 const outfitRoutes = require("./routes/outfit-routes");
 const itemRoutes = require("./routes/item-routes");
 
-// HTTPS: set up
-const path = require("path");
-const fs = require("fs");
-const https = require("https");
-// HTTPS: certifications
-const certification = {
-  key: fs.readFileSync(path.resolve("./ssl/server.key")),
-  cert: fs.readFileSync(path.resolve("./ssl/server.crt"))
-};
+// // HTTPS: set up
+// const path = require("path");
+// const fs = require("fs");
+// const https = require("https");
+// // HTTPS: certifications
+// const certification = {
+//   key: fs.readFileSync(path.resolve("./ssl/server.key")),
+//   cert: fs.readFileSync(path.resolve("./ssl/server.crt"))
+// };
 
 // set up server
 const server = express();
@@ -96,7 +96,6 @@ server.get("/", (req, res) => {
 });
 
 // Add a new user to the database
-// QUESTION: Is this being used anywhere??
 server.post("/signup", (req, res) => {
   const { username, password, email } = req.body;
   User.create({ username, password, email })
@@ -109,8 +108,8 @@ server.post("/signup", (req, res) => {
     })
 });
 
-//HTTPS: server start
-https.createServer(certification, server).listen(5000);
+// //HTTPS: server start
+// https.createServer(certification, server).listen(5000);
 
 // Start the server
 server.listen(port, () => {
