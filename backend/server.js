5c7646b3e5b8e1fd28a61718952a8b1cb8d6da6f
Nedu Robert <nedumrobert@gmail.com>
2018-08-23 14:40:01 -0700

    Commented out unneeded code for now

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,14 @@ const helmet = require("helmet");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const port = process.env.PORT || 5001; // HTTPS: changed port to use https on 5000
+const port = process.env.PORT || 5000; // HTTPS: changed port to use https on 5000
 const User = require("./models/userModel");
 
 require("dotenv").config();
 
-const cookieSession = require("cookie-session");
+// const cookieSession = require("cookie-session");
 const passport = require("passport");
-//const passportSetup = require("./config/passport-setup");
+// const passportSetup = require("./config/passport-setup");
 
 const localAuthRoutes = require("./routes/local-auth-routes");
 const authRoutes = require("./routes/auth-routes");
@@ -21,14 +21,14 @@ const outfitRoutes = require("./routes/outfit-routes");
 const itemRoutes = require("./routes/item-routes");
 
 // HTTPS: set up
-const path = require("path");
-const fs = require("fs");
-const https = require("https");
+// const path = require("path");
+// const fs = require("fs");
+// const https = require("https");
 // HTTPS: certifications
-const certification = {
-  key: fs.readFileSync(path.resolve("./ssl/server.key")),
-  cert: fs.readFileSync(path.resolve("./ssl/server.crt"))
-};
+// const certification = {
+//   key: fs.readFileSync(path.resolve("./ssl/server.key")),
+//   cert: fs.readFileSync(path.resolve("./ssl/server.crt"))
+// };
 
 // set up server
 const server = express();
@@ -41,15 +41,15 @@ const corsOptions = {
 server.use(cors(corsOptions));
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+// server.use(cors());
 
-//set up cookie-session
-server.use(
-  cookieSession({
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [process.env.COOKIE_KEY]
-  })
-);
+// //set up cookie-session
+// server.use(
+//   cookieSession({
+//     maxAge: 24 * 60 * 60 * 1000,
+//     keys: [process.env.COOKIE_KEY]
+//   })
+// );
 
 // const upload = multer({
 //   dest: './uploads/',
@@ -59,19 +59,19 @@ server.use(
 // });
 
 // set up passport. Initialize
-server.use(passport.initialize());
-server.use(passport.session());
+// server.use(passport.initialize());
+// server.use(passport.session());
 
 // Allow passport to utilize sessions
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
 
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
-});
+// passport.deserializeUser((id, done) => {
+//   User.findById(id, (err, user) => {
+//     done(err, user);
+//   });
+// });
 
 // set up routes
 server.use("/local-auth", localAuthRoutes);
@@ -97,19 +97,21 @@ server.get("/", (req, res) => {
 
 // Add a new user to the database
 // QUESTION: Is this being used anywhere??
-server.post("/signup", (req, res) => {
-  const { username, password, email } = req.body;
-  User.create({ username, password, email })
-    .then(user => {
-      passport.authenticate('local', { successRedirect: '/' });
-      res.status(201).json(user);
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message });
-    })
-});
+// server.post("/signup", (req, res) => {
+//   const { username, password, email } = req.body;
+//   User.create({ username, password, email })
+//     .then(user => {
+//       passport.authenticate('local', { successRedirect: '/' });
+//       res.status(201).json(user);
+//     })
+//     .catch(err => {
+//       res.status(500).json({ error: err.message });
+//     })
+// });
 
 //HTTPS: server start
+// https.createServer(certification, server).listen(5000);
+
 // Catch-all error handler
 server.use((err, req, res, next) => {
   console.log(err);
