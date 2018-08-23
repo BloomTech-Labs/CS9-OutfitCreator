e19e7dfc34e3066853572aee2a8c5349439bff4c
Justin M <jekm321@gmail.com>
2018-08-23 11:30:54 -0400

    renamed code to make sense, old name was just for testing period

--- backend/routes/item-routes.js
+++ backend/routes/item-routes.js
@@ -22,10 +22,10 @@ const storage = multer.diskStorage({
   }
 });
 
-const jekmsUpload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 // const upload = multer({ dest: './uploads' });
 // Add a new item to the database
-router.post("/", jekmsUpload.single("image"), (req, res) => {
+router.post("/", upload.single("image"), (req, res) => {
   // console.log('req.body: ' + req.body);
   const { user, name, type, tags } = req.body;
   const { originalname } = req.file;
