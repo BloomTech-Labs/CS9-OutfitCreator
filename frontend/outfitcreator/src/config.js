3246c8aa82ba2dcc1dfa04840d56bdd8bf37f213
Devon Bacon <devon.bacon@gmail.com>
2018-08-22 20:41:57 -0700

    small deployment fixes

--- frontend/outfitcreator/src/config.js
+++ frontend/outfitcreator/src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  ROOT_URL: process.env.NODE_ENV === 'production' ?
+  ROOT_URL: process.env.REACT_APP_NODE_ENV === 'production' ?
   { 
-    WEB: 'https://lambda-outfit-creator.herokuapp.com/',
-    API: 'https://lambda-outfit-creator-api.herokuapp.com/'
+    WEB: 'https://lambda-outfit-creator.herokuapp.com',
+    API: 'https://lambda-outfit-creator-api.herokuapp.com'
   } : {
     WEB: 'http://localhost:3000',
     API: 'http://localhost:5000'
