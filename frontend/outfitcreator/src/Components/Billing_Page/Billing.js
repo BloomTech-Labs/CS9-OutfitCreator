7dbffa3bcba58977a21bb00fbb4ec5e817d5d620
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-08-27 09:10:56 -0700

    save changes for merge with origin master

--- frontend/outfitcreator/src/Components/Billing_Page/Billing.js
+++ frontend/outfitcreator/src/Components/Billing_Page/Billing.js
@@ -2,16 +2,27 @@ import './billing.css';
 import React from 'react';
 import Checkout from './Checkout';
 import Cancel from './Cancel';
+import axios from 'axios';
 
 class Billing extends React.Component {
     constructor() {
         super();
         this.state = {
             stripe: null,
-            subscribed: false,
+            userID: null,
+            subscribed: null,
+            subscription: null,
         };
     }
+
     componentDidMount() {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+            var base64Url = token.split('.')[1];
+            var base64 = base64Url.replace('-', '+').replace('_', '/');
+            const userdata = JSON.parse(window.atob(base64));
+            this.setState({ userID: userdata.sub });
+        }
         // get user info from server to see if user is subscribed
         if (window.Stripe) {
             this.setState({stripe: window.Stripe("pk_test_vRQk70zZL34BhEqLJJtqp29z")})
@@ -24,12 +35,11 @@ class Billing extends React.Component {
     
     render() {
             //TODO: change subscription prop for Cancel button to a user profile reference.
-            //TODO: conditional rendering -- show Cancel if already subscribed, otherwise Checkout
             return (
                 <div className='container--billing'>
-                    {this.state.subscribed
-                    ?<Cancel stripe = {this.state.stripe} subscription='sub_DSE0Hm8zCEYQNK'/>
-                    :<Checkout stripe={this.state.stripe}/>
+                    {(this.state.subscribed == (false || null))
+                    ?<Checkout stripe={this.state.stripe}/>
+                    :<Cancel stripe = {this.state.stripe} subscription={this.state.subscription}/>
                     }
                 </div>
             )};
