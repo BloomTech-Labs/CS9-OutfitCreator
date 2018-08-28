94b878b7e15ba719ab3ebbc2160010d296481bb5
Devon Bacon <devon.bacon@gmail.com>
2018-08-28 15:53:30 -0700

    Settings.js - changed settings to stateful component

--- frontend/outfitcreator/src/Components/Settings_Page/Settings.js
+++ frontend/outfitcreator/src/Components/Settings_Page/Settings.js
@@ -1,56 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
-
+import { ROOT_URL } from '../../config';
+import axios from 'axios';
 import './Settings.css';
 
-const Settings = (props) => {
-    return (
-        <div className="settingsContainer">
-          <Form>
-            <FormGroup row>
-              <Label for="userEmail" sm={4}>Email:</Label>
-              <Col sm={8}>
-                <Input type="email" name="email" id="userEmail" placeholder="user@gmail.com" />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="userPhone" sm={4}>Phone:</Label>
-              <Col sm={8}>
-                <Input type="text" name="phone" id="userPhone" placeholder="555-789-1234" />
-              </Col>
-            </FormGroup>
-            <FormGroup check inline>
-              <Label check>
-                <Input type="checkbox" />
-                Emails?
-              </Label>
-            </FormGroup>
-            <FormGroup check inline>
-              <Label check>
-                <Input type="checkbox" />
-                Texts?
-              </Label>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="userOldPassword" sm={5}>Old Password</Label>
-              <Col sm={7}>
-                <Input type="password" name="oldPassword" id="userOldPassword" placeholder="Enter old password" />
-              </Col>
-            </FormGroup>
-            <FormGroup row>
-              <Label for="usernewPassword" sm={5}>New Password</Label>
-              <Col sm={7}>
-                <Input type="password" name="newPassword" id="userNewPassword" placeholder="Enter new password" />
-              </Col>
-            </FormGroup>
-            <FormGroup check row>
-              <Col sm={{ size: 10, offset: 2 }}>
-                <Button>Save</Button>
-              </Col>
-            </FormGroup>
-          </Form>
-        </div>
-    );
+class Settings extends Component {
+    render() {
+        return (
+            <div className="settingsContainer">
+                <Form>
+                  <FormGroup row>
+                      <Label for="userEmail" sm={4}>Email:</Label>
+                      <Col sm={8}>
+                          <Input 
+                              type="email" 
+                              name="email" 
+                              id="userEmail" 
+                              placeholder="user@gmail.com"
+                              value={ this.state ? this.state.email : '' }
+                              onChange={this.handleInputChange}
+                          />
+                      </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                      <Label for="userPhone" sm={4}>Phone:</Label>
+                      <Col sm={8}>
+                          <Input 
+                              type="text" 
+                              name="phone" 
+                              id="userPhone" 
+                              placeholder="555-789-1234"
+                              value={ this.state ? this.state.phone : '' }
+                              onChange={this.handleInputChange}
+                          />
+                      </Col>
+                  </FormGroup>
+                  <FormGroup check inline>
+                      <Label check>
+                          <Input 
+                              type="checkbox"
+                              name="rEmails"
+                              checked={this.state ? this.state.rEmails : false }
+                              onClick={this.handleInputChange}
+                          />
+                          Emails?
+                      </Label>
+                  </FormGroup>
+                  <FormGroup check inline>
+                      <Label check>
+                          <Input 
+                              type="checkbox"
+                              name="rTexts"
+                              checked={this.state ? this.state.rTexts : false }
+                              onClick={this.handleInputChange}
+                          />
+                          Texts?
+                      </Label>
+                  </FormGroup>
+                  <FormGroup row>
+                      <Label for="userOldPassword" sm={5}>Old Password</Label>
+                      <Col sm={7}>
+                          <Input 
+                              type="password" 
+                              name="oldPassword" 
+                              id="userOldPassword" 
+                              placeholder="Enter old password"
+                              onChange={this.handleInputChange} 
+                          />
+                      </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                      <Label for="usernewPassword" sm={5}>New Password</Label>
+                      <Col sm={7}>
+                          <Input 
+                              type="password" 
+                              name="newPassword" 
+                              id="userNewPassword" 
+                              placeholder="Enter new password"
+                              onChange={this.handleInputChange} 
+                          />
+                      </Col>
+                  </FormGroup>
+                  <FormGroup check row>
+                      <Col sm={{ size: 10, offset: 2 }}>
+                          <Button>Save</Button>
+                      </Col>
+                    </FormGroup>
+                </Form>
+            </div>
+        )
+    }
 };
 
 export default Settings;
