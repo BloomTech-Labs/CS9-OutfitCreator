49bda1721fa999e346554e3b8ca9122f7619a3f1
Devon Bacon <devon.bacon@gmail.com>
2018-08-27 23:23:55 -0700

    Created dresser nav

--- frontend/outfitcreator/src/Components/Navigation/Navigation.js
+++ frontend/outfitcreator/src/Components/Navigation/Navigation.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
-import { Nav, NavItem, NavLink, Button, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Collapse, Nav, NavLink, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { withRouter } from 'react-router';
-import { ROOT_URL } from '../../config';
 import './Navigation.css';
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    const navMin = document.querySelector('.navigation--minimize');
+    const sideNav = document.querySelector('.SideNav');
+    const delay = 200;
+
+    if (this.state.collapsed) {
+      navMin.classList.toggle('change');
+      sideNav.style.top = '40px';
+      setTimeout(() => {
+        navMin.classList.toggle('cross');
+      }, delay);
+    } else {
+      navMin.classList.toggle('cross');
+      setTimeout(() => {
+        sideNav.style.top = '0px';
+        navMin.classList.toggle('change');
+      }, delay);
+    }
+
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
   signOut = () => {
     localStorage.removeItem('authToken');
   }
@@ -16,13 +48,22 @@ class Navigation extends Component {
           <BreadcrumbItem tag="a" href="/">Home</BreadcrumbItem>
           <BreadcrumbItem active tag="span">{this.props.location.pathname.slice(1)}</BreadcrumbItem>
         </Breadcrumb>
+        <div className='navigation--dresser'>
+           <div className='navigation--minimize' onClick={this.toggleNavbar}>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+          </div>
+        </div>
         <Nav className='SideNav'>
-          <NavLink href='/Create' className='Create'>Create</NavLink>
-          <NavLink href='/Upload' className='Upload'>Upload</NavLink>
-          <NavLink href='/Archive' className='Archive'>Archive</NavLink>
-          <NavLink href='/Settings' className='Settings'>Settings</NavLink>
-          <NavLink href='/Billing' className='Billing'>Billing</NavLink>
-          <NavLink href='/' className='SignOut' onClick={this.signOut}>Sign Out</NavLink>
+          <Collapse isOpen={!this.state.collapsed}>
+            <NavLink href='/Create' className='Create'>Create</NavLink>
+            <NavLink href='/Upload' className='Upload'>Upload</NavLink>
+            <NavLink href='/Archive' className='Archive'>Archive</NavLink>
+            <NavLink href='/Settings' className='Settings'>Settings</NavLink>
+            <NavLink href='/Billing' className='Billing'>Billing</NavLink>
+            <NavLink href='/' className='SignOut' onClick={this.signOut}>Sign Out</NavLink>
+          </Collapse>
         </Nav>
       </div>
     );
