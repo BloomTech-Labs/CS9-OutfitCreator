cbfe3aea3371b53c8e9a6122b1a7c80573916299
Nedu Robert <nedumrobert@gmail.com>
2018-08-30 08:57:57 -0700

    Integrated facebook oauth to use jwt

--- backend/config/passport-setup.js
+++ backend/config/passport-setup.js
@@ -9,7 +9,7 @@ const GitHubStrategy = require("passport-github2").Strategy;
 
 require("dotenv").config();
 
-const Guser = require("../models/gusermodel");
+// const Guser = require("../models/gusermodel");
 const User = require("../models/userModel");
 const secret = process.env.SECRET || "SECRET!";
 
@@ -96,36 +96,47 @@ const googleStrategy = new GoogleStrategy({
   });
 });
 
-// passport.use(
-//   new FacebookStrategy(
-//     {
-//       callbackURL: "https://localhost:5000/auth/facebook/callback",
-//       clientID: process.env.FB_CLIENT_ID,
-//       clientSecret: process.env.FB_CLIENT_SECRET,
-//       profileFields: ["id", "displayName", "name", "gender", "photos", "email"]
-//     },
-//     (accessToken, refreshToken, profile, done) => {
-//       Guser.findOne({ "facebook.facebookId": profile.id }).then(currentUser => {
-//         if (currentUser) {
-//           done(null, currentUser);
-//         } else {
-//           new Guser({
-//             "facebook.facebookId": profile.id,
-//             "facebook.username": profile.displayName,
-//             "facebook.picture": profile._json.picture.data.url,
-//             "facebook.token": accessToken,
-//             "facebook.email": profile.emails[0].value
-//           })
-//             .save()
-//             .then(newUser => {
-//               done(null, newUser);
-//             });
-//         }
-//       });
-//     }
-//   )
-// );
-
+const facebookStrategy = new FacebookStrategy(
+  {
+    callbackURL: "/auth/facebook/callback",
+    clientID: process.env.FB_CLIENT_ID,
+    clientSecret: process.env.FB_CLIENT_SECRET,
+    profileFields: ["id", "displayName", "name", "gender", "photos", "email"]
+  },
+  (accessToken, refreshToken, profile, done) => {
+    User.findOne({ $or: [{"facebook.id": profile.id }, {"local.email": profile.emails[0].value }]}).then(existingUser => {
+      if (existingUser) {
+        if(existingUser.facebook.id == undefined) {
+          existingUser.facebook.id = profile.id;
+          existingUser.facebook.username = profile.displayName;
+          existingUser.facebook.email = profile.emails[0].value;
+          existingUser.facebook.picture = profile._json.picture.data.url;
+          existingUser.facebook.token = accessToken;
+          existingUser.save();
+        }
+        done(null, existingUser);
+      } else {
+        let newUser = new User({
+          method: 'facebook',
+          facebook: {
+            id: profile.id,
+            username: profile.displayName,
+            email: profile.emails[0].value,
+            picture: profile._json.picture.data.url,
+            token: accessToken
+          }
+        })
+        newUser.local.email = profile.emails[0].value;
+        newUser.save()
+        .then(newUser => {
+          done(null, newUser);
+        });
+      }
+    }).catch(err => {
+      done(err, false, err.message);
+    })
+  }
+)
 // passport.use(
 //   new GitHubStrategy(
 //     {
@@ -159,14 +170,19 @@ const googleStrategy = new GoogleStrategy({
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 passport.use(googleStrategy);
+passport.use(facebookStrategy)
 
 // passport local middleware
 const passportOptions = { session: false };
 const googleOptions = { session: false, scope: ["profile", "email"]};
+const facebookOptions = { session: false, scope: ["email"]};
+const facebookRedirectOptions = { session: false, successRedirect: "/profile", failureRedirect: "/login" };
 const authenticate = passport.authenticate('local', passportOptions);
 const restricted = passport.authenticate('jwt', passportOptions);
 const googleAuthenticate = passport.authenticate('google', googleOptions);
+const facebookAuthenticate = passport.authenticate('facebook', facebookOptions);
 const googleRedirectAuthenticate = passport.authenticate('google', passportOptions);
+const facebookRedirectAuthenticate = passport.authenticate('google', facebookRedirectOptions);
 
 function makeToken(user) {
   console.log('makeToken: ',user._id);
@@ -195,12 +211,12 @@ const signToken = (req, res) => {
     expiresIn: '24h'
   };
   jwt.sign(payload, secret, options, (err, token) => {
-      if(err){
-          res.sendStatus(500);
-      } else {
-          res.json({token});
-      }
+    if(err){
+        res.sendStatus(500);
+    } else {
+        res.json({token});
+    }
   });
 }
 
-module.exports = { authenticate, restricted, googleAuthenticate, googleRedirectAuthenticate, makeToken, signToken }
+module.exports = { authenticate, restricted, googleAuthenticate, googleRedirectAuthenticate, facebookAuthenticate, facebookRedirectAuthenticate, makeToken, signToken }
