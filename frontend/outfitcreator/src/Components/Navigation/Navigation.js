b443c2e3d4ee13f2081a9e49310b7b75f2abc0f2
Devon Bacon <devon.bacon@gmail.com>
2018-08-30 18:21:22 -0700

    Improved navigation

--- frontend/outfitcreator/src/Components/Navigation/Navigation.js
+++ frontend/outfitcreator/src/Components/Navigation/Navigation.js
@@ -15,15 +15,19 @@ class Navigation extends Component {
   constructor(props) {
     super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
+    // this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
       collapsed: true,
+      collapseActive: false,
       username: ''
     };
     this.user = '';
   }
 
   componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.updateDimensions);
+
     const token = this.props.tokenData();
 
     if (token) {
@@ -43,9 +47,24 @@ class Navigation extends Component {
     }
   }
 
-  toggleNavbar() {
+  updateDimensions= () => {
+    if (window.innerWidth < 700 && this.state.collapseActive === false) {
+      this.setState({ collapseActive: true });
+    } else if (window.innerWidth > 699 && this.state.collapseActive === true) {
+      this.setState({ collapsed: true, collapseActive: false });
+    }
+    // if(window.innerWidth < 500) {
+    //   this.setState({ width: 450, height: 102 });
+    // } else {
+    //   let update_width  = window.innerWidth-100;
+    //   let update_height = Math.round(update_width/4.4);
+    //   this.setState({ width: update_width, height: update_height });
+    // }
+  }
+
+  toggleNavbar = () => {
     const navMin = document.querySelector('.navigation--minimize');
-    const sideNav = document.querySelector('.navigation--sideNav');
+    const sideNav = document.querySelector('.navigation--pages');
     const delay = 250;
 
     if (this.state.collapsed) {
@@ -67,36 +86,59 @@ class Navigation extends Component {
     });
   }
 
-  signOut = () => {
+  navSettings() {
+    window.location = `${ROOT_URL.WEB}/Settings`;
+  }
+
+  signOut() {
     localStorage.removeItem('authToken');
     window.location = `${ROOT_URL.WEB}/`;
   }
 
   render() {
     return (
+      this.state.collapseActive ?
       <div className='navigation--container'>
-        <div className='navigation--dresser'>
-          <div className='navigation--minimize' onClick={this.toggleNavbar}>
-            <div className="bar1"></div>
-            <div className="bar2"></div>
-            <div className="bar3"></div>
-          </div>
+        <div className='navigation--minimize' onClick={this.toggleNavbar}>
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
         </div>
-        <Nav className='navigation--sideNav'>
+        <Nav className='navigation--pages'>
           <Collapse isOpen={!this.state.collapsed}>
             <NavLink href='/Create'><button className='nav--item'>New Outfit</button></NavLink>
             <NavLink href='/Upload'><button className='nav--item'>Add Item</button></NavLink>
             <NavLink href='/Closet'><button className='nav--item'>My Closet</button></NavLink>
             <NavLink href='/Archive'><button className='nav--item'>Archive</button></NavLink>
-            <NavLink href='/Settings'><button className='nav--item'>Settings</button></NavLink>
-            <NavLink href='/Billing'><button className='nav--item'>Billing</button></NavLink>
           </Collapse>
         </Nav>
         <div className='navigation--topRight'>
-          <div className='navigation--user'>{this.state.username}</div>
+          <div onClick={this.navSettings} className='navigation--user'>{this.state.username}</div>
           <div onClick={this.signOut} className='navigation--logout'>logout</div>
         </div>
+      </div> 
+      :
+      <div className='navigation--container'>
+      {/* <div className='navigation--dresser'>
+        <div className='navigation--minimize' onClick={this.toggleNavbar}>
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+        </div>
+      </div> */}
+      <Nav className='navigation--pages'>
+        {/* <Collapse isOpen={!this.state.collapsed}> */}
+          <NavLink href='/Create'><button className='nav--item'>New Outfit</button></NavLink>
+          <NavLink href='/Upload'><button className='nav--item'>Add Item</button></NavLink>
+          <NavLink href='/Closet'><button className='nav--item'>My Closet</button></NavLink>
+          <NavLink href='/Archive'><button className='nav--item'>Archive</button></NavLink>
+        {/* </Collapse> */}
+      </Nav>
+      <div className='navigation--topRight'>
+        <div onClick={this.navSettings} className='navigation--user'>{this.state.username}</div>
+        <div onClick={this.signOut} className='navigation--logout'>logout</div>
       </div>
+    </div>
     );
   }
 }
