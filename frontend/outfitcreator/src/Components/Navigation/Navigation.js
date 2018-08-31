4bcbadde3d5f73f41a67e4ed5657740fff36e631
Devon Bacon <devon.bacon@gmail.com>
2018-08-30 21:01:59 -0700

    Made nav completely reactive and added current page indicator

--- frontend/outfitcreator/src/Components/Navigation/Navigation.js
+++ frontend/outfitcreator/src/Components/Navigation/Navigation.js
@@ -25,6 +25,7 @@ class Navigation extends Component {
   }
 
   componentDidMount() {
+    this.selectActivePage();
     this.updateDimensions();
     window.addEventListener("resize", this.updateDimensions);
 
@@ -53,13 +54,22 @@ class Navigation extends Component {
     } else if (window.innerWidth > 699 && this.state.collapseActive === true) {
       this.setState({ collapsed: true, collapseActive: false });
     }
-    // if(window.innerWidth < 500) {
-    //   this.setState({ width: 450, height: 102 });
-    // } else {
-    //   let update_width  = window.innerWidth-100;
-    //   let update_height = Math.round(update_width/4.4);
-    //   this.setState({ width: update_width, height: update_height });
-    // }
+  }
+
+  selectActivePage() {
+    const activePage = this.props.match.path.slice(1);
+    const activePageLink = document.querySelector(`.${activePage}`);
+    const pageLinks = document.querySelectorAll('.nav--item');
+
+    console.log(pageLinks);
+
+    pageLinks.forEach(link => {
+      if (link.classList.contains(activePage)) {
+        link.classList.add('nav--active');
+      } else {
+        link.classList.remove('nav--active');
+      }
+    });
   }
 
   toggleNavbar = () => {
@@ -68,6 +78,7 @@ class Navigation extends Component {
     const delay = 250;
 
     if (this.state.collapsed) {
+      this.selectActivePage();
       navMin.classList.toggle('change');
       sideNav.classList.toggle('sideNav--expanded');
       setTimeout(() => {
@@ -106,33 +117,24 @@ class Navigation extends Component {
         </div>
         <Nav className='navigation--pages'>
           <Collapse isOpen={!this.state.collapsed}>
-            <NavLink href='/Create'><button className='nav--item'>New Outfit</button></NavLink>
-            <NavLink href='/Upload'><button className='nav--item'>Add Item</button></NavLink>
-            <NavLink href='/Closet'><button className='nav--item'>My Closet</button></NavLink>
-            <NavLink href='/Archive'><button className='nav--item'>Archive</button></NavLink>
+            <NavLink href='/Create'><button className='nav--item Create nav--active'>New Outfit</button></NavLink>
+            <NavLink href='/Upload'><button className='nav--item Upload'>Add Item</button></NavLink>
+            <NavLink href='/Closet'><button className='nav--item Closet'>My Closet</button></NavLink>
+            <NavLink href='/Archive'><button className='nav--item Archive'>Archive</button></NavLink>
           </Collapse>
         </Nav>
         <div className='navigation--topRight'>
           <div onClick={this.navSettings} className='navigation--user'>{this.state.username}</div>
           <div onClick={this.signOut} className='navigation--logout'>logout</div>
         </div>
-      </div> 
+      </div>
       :
       <div className='navigation--container'>
-      {/* <div className='navigation--dresser'>
-        <div className='navigation--minimize' onClick={this.toggleNavbar}>
-          <div className="bar1"></div>
-          <div className="bar2"></div>
-          <div className="bar3"></div>
-        </div>
-      </div> */}
       <Nav className='navigation--pages'>
-        {/* <Collapse isOpen={!this.state.collapsed}> */}
-          <NavLink href='/Create'><button className='nav--item'>New Outfit</button></NavLink>
-          <NavLink href='/Upload'><button className='nav--item'>Add Item</button></NavLink>
-          <NavLink href='/Closet'><button className='nav--item'>My Closet</button></NavLink>
-          <NavLink href='/Archive'><button className='nav--item'>Archive</button></NavLink>
-        {/* </Collapse> */}
+      <NavLink href='/Create'><button className='nav--item Create'>New Outfit</button></NavLink>
+            <NavLink href='/Upload'><button className='nav--item Upload'>Add Item</button></NavLink>
+            <NavLink href='/Closet'><button className='nav--item Closet'>My Closet</button></NavLink>
+            <NavLink href='/Archive'><button className='nav--item Archive'>Archive</button></NavLink>
       </Nav>
       <div className='navigation--topRight'>
         <div onClick={this.navSettings} className='navigation--user'>{this.state.username}</div>
