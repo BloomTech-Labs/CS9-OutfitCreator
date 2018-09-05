458e6fdacec07ab559e8dc630709c0eb74abeacc
Nedu Robert <nedumrobert@gmail.com>
2018-09-04 22:28:54 -0700

    Implemented sending of verification email to newly registered users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,7 @@
 const User = require("../models/userModel");
 const { makeToken } = require("../config/passport-setup");
+const { generateSignupKey, sendEmail } = require('../config/mailer');
+const { ROOT_URL } = require('../config/keys');
 
 // Register a new user
 exports.signup = (req, res) => {
@@ -33,49 +35,47 @@ exports.signup = (req, res) => {
                 })
             }
         } else {
-            
+            const key = generateSignupKey();
+            const user = new User({
+                method: 'local',
+                local: {
+                    username,
+                    password,
+                    email
+                },
+                signupKey: key
+            });
+            user.save().then(user => {                
+                // Send verification email
+                const subject = "Closet Roulette | Email Verification Required";
+                const url = `${ROOT_URL.WEB}/verify/${key.key}`;
+                const html = `Hi there,
+                <br/>
+                Thank you for registering!
+                <br/><br/>
+                Please verify your email by clicking this link: <a href="${url}">${url}</a>
+                <br/>
+                Have a pleasant day.`;
+                const text = `Please click here to verify your email: ${url}`;
+                sendEmail(email, subject, html, text)
+                .then(() => {
+                console.log('email sent (authentication.js > 124)');
+                })
+                .catch((err) => {
+                console.log(err);
+                return next(err);
+                });
+                    
+                const token = makeToken(user);
+                res.status(201).json({ token });
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
         }
-    })
-    const user = new User({
-        method: 'local',
-        local: {
-            username,
-            password,
-            email
-        }
-    });
-    user.save().then(user => {
-        console.log(user)
-        const token = makeToken(user);
-        res.status(200).json({ token });
-    })
-    .catch(err => {
-        res.status(500).json(err);
     })
 };
 
-// Login
-exports.login = (req, res) => {
-    res.status(200).json({ token: makeToken(req.user), user: req.user })
-}
-
-// Logout
-exports.logout = (req, res) => {
-    // if (req.session) {
-    //     req.session.destroy(err => {
-    //         if(err) res.status(500).json({ error: 'error logging out' });
-    //         else res.status(200).send('Logged Out');
-    //     });
-    // } else {
-    //     res.send('User not logged in.');
-    // }
-    // const token = req.headers.authorization.slice(7)
-    // User.findOne({ token })
-    // .then(user => user.update({ $set: { token: null } })
-    //   .then(() => res.send('Logged out successfully')))
-    // .catch(err => next(err))
-    console.log(req.headers);
-}
 // Login
 exports.login = (req, res) => { 
     res.status(200).json({ token: makeToken(req.user), user: req.user })
