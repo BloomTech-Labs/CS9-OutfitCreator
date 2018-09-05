9c84f688ff148a5a77d9374c1a87ea68411ba39f
Nedu Robert <nedumrobert@gmail.com>
2018-09-04 22:26:45 -0700

    Implemented verifyEmail method

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -76,3 +76,51 @@ exports.logout = (req, res) => {
     // .catch(err => next(err))
     console.log(req.headers);
 }
+// Login
+exports.login = (req, res) => { 
+    res.status(200).json({ token: makeToken(req.user), user: req.user })
+}
+
+// Logout
+exports.logout = (req, res) => {
+    // if (req.session) {
+    //     req.session.destroy(err => {
+    //         if(err) res.status(500).json({ error: 'error logging out' });
+    //         else res.status(200).send('Logged Out');
+    //     });
+    // } else {
+    //     res.send('User not logged in.');
+    // }
+    // const token = req.headers.authorization.slice(7)
+    // User.findOne({ token })
+    // .then(user => user.update({ $set: { token: null } })
+    //   .then(() => res.send('Logged out successfully')))
+    // .catch(err => next(err))
+    console.log(req.headers);
+}
+
+exports.verifyEmail = (req, res) => {
+    const key = req.body.key;
+    const target = {
+      'signupKey.key': key,
+      'signupKey.exp': { $gt: Date.now() }
+      };
+    const updates = {
+      verified: true
+    };
+    const options = { new: true };
+  
+    User.findOneAndUpdate(target, updates, options)
+      .exec()
+      .then( user => {
+      if (!user) {
+        return res.status(400).json({ message: 'Sorry, your token has expired, please try again.' });
+      } else {
+        const token = makeToken(user);
+        res.status(201).json({ token });
+      }
+      })
+      .catch( err => {
+        return res.status(400).json({ message: err});
+      });
+    }
