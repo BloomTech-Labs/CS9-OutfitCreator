e80d667383986f4e995942293165e2f6320e9a11
Nedu Robert <nedumrobert@gmail.com>
2018-09-07 12:20:49 -0700

    Implemented github OAuth route and cleaned up file

--- backend/routes/auth-routes.js
+++ backend/routes/auth-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { authenticate, googleAuthenticate, googleRedirectAuthenticate, facebookAuthenticate, facebookRedirectAuthenticate, signToken } = require("../config/passport-setup");
+const { authenticate, googleAuthenticate, googleRedirectAuthenticate, facebookAuthenticate, facebookRedirectAuthenticate, githubAuthenticate, githubRedirectAuthenticate, signToken } = require("../config/passport-setup");
 const users = require("../controllers/users");
 
 
@@ -10,20 +10,8 @@ router.get('/google', googleAuthenticate);
 router.get('/google/redirect', googleRedirectAuthenticate, signToken);
 router.get('/facebook', facebookAuthenticate);
 router.get('/facebook/callback', facebookRedirectAuthenticate, signToken);
+router.get('/github', githubAuthenticate);
+router.get('/github/callback', githubRedirectAuthenticate, signToken);
 router.post('/verify', users.verifyEmail)
 
-// // auth with github
-// router.get(
-//   "/github",
-//   passport.authenticate("github", {
-//     scope: ["user"]
-//   })
-// );
-
-// // callback route for github to redirect to
-// router.get("/github/callback", passport.authenticate("github"), (req, res) => {
-//   res.redirect("/profile");
-// });
-
-// Google/Facebook/Github buttons to be implemented in React page
 module.exports = router;
