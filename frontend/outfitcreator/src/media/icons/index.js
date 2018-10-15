6ccab69ffd296e93a90773ec6551d4f73ef4113a
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-10-14 20:10:17 -0700

    media/icons - imports all icons and creates index

---
+++ frontend/outfitcreator/src/media/icons/index.js
@@ -0,0 +1,33 @@
+import bottom from './bottom.png';
+import casualShoes from './casualShoes.png';
+import dress from './dress.png';
+import formalShoes from './formalShoes.png';
+import jacket from './jacket.png';
+import leggings from './leggings.png';
+import pants from './pants.png';
+import shirt from './shirt.png';
+import shorts from './shorts.png';
+import skirt from './skirt.png';
+import sweater from './sweater.png';
+import top from './top.png';
+import locked from './locked.png';
+import unlocked from './unlocked.png';
+import refresh from './refresh-button.png';
+
+export const Icons = {
+  bottom,
+  casualShoes,
+  dress, 
+  formalShoes,
+  jacket,
+  leggings,
+  pants,
+  shirt,
+  shorts,
+  skirt,
+  sweater,
+  top,
+  locked,
+  unlocked,
+  refresh,
+}
