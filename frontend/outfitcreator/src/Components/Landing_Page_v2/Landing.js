a75872740fed8db0709e12653d267541e11dd358
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-10-15 22:57:25 -0700

    Landing.js - refactors for uniform guidelines

--- frontend/outfitcreator/src/Components/Landing_Page_v2/Landing.js
+++ frontend/outfitcreator/src/Components/Landing_Page_v2/Landing.js
@@ -45,50 +45,62 @@ class Landing extends Component {
 					<div className="landing-form">
 						{this.state.signin ? (
 							<TextField
+                className="landing-email landing-input"
                 id="landing-email"
                 label="Email"
-                className="landing-email landing-input"
-                value={this.state.email}
-                onChange={this.handleChange('email')}
                 margin="normal"
+                onChange={this.handleChange('email')}
+                value={this.state.email}
               />
 						) : null}
             <TextField
+              className="landing-username landing-input"
               id="landing-username"
               label="Username"
-              className="landing-username landing-input"
-              value={this.state.username}
-              onChange={this.handleChange('username')}
               margin="normal"
+              onChange={this.handleChange('username')}
+              value={this.state.username}
             />
              <TextField
+              className="landing-password landing-input"
               id="landing-password"
               label="Password"
-              className="landing-password landing-input"
-              value={this.state.password}
-              onChange={this.handleChange('password')}
               margin="normal"
+              onChange={this.handleChange('password')}
+              value={this.state.password}
             />
 						{this.state.signin ? (
 							<TextField
+                className="landing-vpassword landing-input"
                 id="landing-vpassword"
                 label="Verify Password"
-                className="landing-vpassword landing-input"
-                value={this.state.vpassword}
-                onChange={this.handleChange('vpassword')}
                 margin="normal"
+                onChange={this.handleChange('vpassword')}
+                value={this.state.vpassword}
               />
             ) : null}
-            <Button variant="outlined" className="landing-button-main">
+            <Button className="landing-button-main" variant="outlined">
               {this.state.signin ? 'Signup' : 'Login'}
             </Button>
             <Button  className="landing-button-sub" onClick={this.toggleSignin}>
               {this.state.signin ? 'Login' : 'Signup'}?
             </Button>
             <div className="landing-oauth">
-              <img className="landing-oauth-icon" src={Icons.google} />
-              <img className="landing-oauth-icon" src={Icons.facebook} />
-              <img className="landing-oauth-icon" src={Icons.github} />
+              <img 
+                alt="google icon"
+                className="landing-oauth-icon" 
+                src={Icons.google} 
+              />
+              <img 
+                alt="facebook icon"
+                className="landing-oauth-icon" 
+                src={Icons.facebook} 
+              />
+              <img 
+                alt="github icon"
+                className="landing-oauth-icon" 
+                src={Icons.github} 
+              />
             </div>
 					</div>
 				</div>
