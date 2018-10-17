bf0f22496395e519fc3f334bf9c445efefc2c3a8
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-10-16 22:57:04 -0700

    Landing.js - includes signUp method from original Landing component

--- frontend/outfitcreator/src/Components/Landing_Page_v2/Landing.js
+++ frontend/outfitcreator/src/Components/Landing_Page_v2/Landing.js
@@ -14,119 +14,136 @@ class Landing extends Component {
 		super(props);
 
 		this.state = {
-			signin: true,
 			email: '',
 			username: '',
 			password: '',
-      vpassword: '',
-      agree: false,
-    };
+			vpassword: '',
+			agree: false,
+			signin: true
+		};
 	}
 
-  handleChange = name => event => {
-    const prop = name === "agree" ? "checked" : "value";
-    this.setState({
-      [name]: event.target[prop],
-    });
-  };
-  
-  handleInput = (event) => {
+	handleChange = (name) => (event) => {
+		const prop = name === 'agree' ? 'checked' : 'value';
+		this.setState({
+			[name]: event.target[prop]
+		});
+	};
+
+	handleInput = (event) => {
 		this.setState({ [event.target.name]: event.target.value });
-  };
-  
-  toggleSignin = () => {
-    this.setState({ signin: !this.state.signin });
-  }
+	};
+
+	toggleSignin = () => {
+		this.setState({ signin: !this.state.signin });
+	};
+
+	signUp = () => {
+		const { username, password, email } = this.state;
+
+		if (this.state.password === this.state.vpassword) {
+			return;
+		}
+
+		if (this.state.agree) {
+			return;
+		}
+
+		if (this.state.agree && this.state.password === this.state.vpassword) {
+			axios
+				.post(`${ROOT_URL.API}/auth/signup`, { username, password, email })
+				.then((res) => {
+					localStorage.setItem('authToken', `Bearer ${res.data.token}`);
+					this.notifySignUpSuccess();
+					this.setState({ activeTab: '2' });
+				})
+				.catch((err) => {
+					this.notifySignUpFailure();
+				});
+		} else {
+			window.alert('Please indicate that you agree to the Terms and Conditions.');
+		}
+	};
 
 	render() {
 		return (
 			<div className="landing-container">
 				<div className="landing-main">
-          <img 
-            alt="closet roulette logo"
-            className="landing-logo" 
-            src={CR_Logo} 
-          />
+					<img alt="closet roulette logo" className="landing-logo" src={CR_Logo} />
 					<div className="landing-form">
 						{this.state.signin ? (
 							<TextField
-                className="landing-email landing-input"
-                id="landing-email"
-                label="Email"
-                margin="normal"
-                onChange={this.handleChange('email')}
-                value={this.state.email}
-              />
+								className="landing-email landing-input"
+								id="landing-email"
+								label="Email"
+								margin="normal"
+								onChange={this.handleChange('email')}
+								value={this.state.email}
+							/>
 						) : null}
-            <TextField
-              className="landing-username landing-input"
-              id="landing-username"
-              label="Username"
-              margin="normal"
-              onChange={this.handleChange('username')}
-              value={this.state.username}
-            />
-             <TextField
-              className="landing-password landing-input"
-              id="landing-password"
-              label="Password"
-              margin="normal"
-              onChange={this.handleChange('password')}
-              type="password"
-              value={this.state.password}
-            />
+						<TextField
+							className="landing-username landing-input"
+							id="landing-username"
+							label="Username"
+							margin="normal"
+							onChange={this.handleChange('username')}
+							value={this.state.username}
+						/>
+						<TextField
+							className="landing-password landing-input"
+							id="landing-password"
+							label="Password"
+							margin="normal"
+							onChange={this.handleChange('password')}
+							type="password"
+							value={this.state.password}
+						/>
 						{this.state.signin ? (
-              <React.Fragment>
-                <TextField
-                  className="landing-vpassword landing-input"
-                  id="landing-vpassword"
-                  label="Verify Password"
-                  margin="normal"
-                  onChange={this.handleChange('vpassword')}
-                  type="password"
-                  value={this.state.vpassword}
-                />
-                <div className="landing-agree">
-                  <Checkbox
-                    checked={this.state.agree}
-                    className="landing-agree-check"
-                    color="default"
-                    onChange={this.handleChange('agree')}
-                    value="agree"
-                  />
-                  <span className="landing-agree-text">I agree that I will only upload images that I own.</span>
-                </div>
-              </React.Fragment>
-            ) : null}
-            <Button className="landing-button-main" variant="outlined">
-              {this.state.signin ? 'Signup' : 'Login'}
-            </Button>
-            <Button  className="landing-button-sub" onClick={this.toggleSignin}>
-              {this.state.signin ? 'Login' : 'Signup'}?
-            </Button>
-            <div className="landing-oauth">
-              <a href={`${ROOT_URL.API}/auth/google`}>
-                <img 
-                  alt="google icon"
-                  className="landing-oauth-icon" 
-                  src={Icons.google} 
-                />
+							<React.Fragment>
+								<TextField
+									className="landing-vpassword landing-input"
+									id="landing-vpassword"
+									label="Verify Password"
+									margin="normal"
+									onChange={this.handleChange('vpassword')}
+									type="password"
+									value={this.state.vpassword}
+								/>
+								<div className="landing-agree">
+									<Checkbox
+										checked={this.state.agree}
+										className="landing-agree-check"
+										color="default"
+										onChange={this.handleChange('agree')}
+										value="agree"
+									/>
+									<span className="landing-agree-text">
+										I agree that I will only upload images that I own.
+									</span>
+								</div>
+							</React.Fragment>
+						) : null}
+						<Button
+							className="landing-button-main"
+							variant="outlined"
+							onClick={this.signin ? this.signIn : this.signUp}
+						>
+							{this.state.signin ? 'Signup' : 'Login'}
+						</Button>
+						<Button className="landing-button-sub" onClick={this.toggleSignin}>
+							{this.state.signin ? 'Login' : 'Signup'}?
+						</Button>
+						<div className="landing-oauth">
+							<a href={`${ROOT_URL.API}/auth/google`}>
+								<img alt="google icon" className="landing-oauth-icon" src={Icons.google} />
 							</a>
-              <a href={`${ROOT_URL.API}/auth/facebook`}>
-                <img 
-                  alt="facebook icon"
-                  className="landing-oauth-icon" 
-                  src={Icons.facebook} 
-                />
-              </a>
-              <a href={`${ROOT_URL.API}/auth/github`}>
-                <img 
-                  alt="github icon"
-                  className="landing-oauth-icon" 
-                  src={Icons.github} 
-                />
+							<a href={`${ROOT_URL.API}/auth/facebook`}>
+								<img alt="facebook icon" className="landing-oauth-icon" src={Icons.facebook} />
 							</a>
-            </div>
+							<a href={`${ROOT_URL.API}/auth/github`}>
+								<img alt="github icon" className="landing-oauth-icon" src={Icons.github} />
+							</a>
+						</div>
 					</div>
 				</div>
 				<div className="landing-info" />
