1a4f3e0600162199b5633350b3a6d92829c622d3
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-10-18 12:18:58 -0700

    Modal.js - created universal modal to be used across entire app

---
+++ frontend/outfitcreator/src/Components/Modal/Modal.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import './Modal.css';
+
+class Modal extends Component {
+	constructor(props) {
+    super(props);
+  }
+
+	render() {
+		return (
+      <div className="modal-background_tinted">
+        <div className="modal-body">
+          <div className="modal-content">{this.props.content}</div>
+          <Button className="mui-button modal-button" onClick={this.props.action} variant="outlined">OK</Button>
+        </div>
+      </div>
+		);
+	}
+}
+
+export default Modal;
