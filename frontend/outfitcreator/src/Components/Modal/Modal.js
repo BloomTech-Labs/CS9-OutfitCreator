5631e260813dbb7a4c5caa80981cf9b86f7b181f
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-10-18 12:35:33 -0700

    Modal.js - formats document with prettier standards

--- frontend/outfitcreator/src/Components/Modal/Modal.js
+++ frontend/outfitcreator/src/Components/Modal/Modal.js
@@ -4,17 +4,19 @@ import './Modal.css';
 
 class Modal extends Component {
 	constructor(props) {
-    super(props);
-  }
+		super(props);
+	}
 
 	render() {
 		return (
-      <div className="modal-background_tinted">
-        <div className="modal-body">
-          <div className="modal-content">{this.props.content}</div>
-          <Button className="mui-button modal-button" onClick={this.props.action} variant="outlined">OK</Button>
-        </div>
-      </div>
+			<div className="modal-background_tinted">
+				<div className="modal-body">
+					<div className="modal-content">{this.props.content}</div>
+					<Button className="mui-button modal-button" onClick={this.props.action} variant="outlined">
+						OK
+					</Button>
+				</div>
+			</div>
 		);
 	}
 }
