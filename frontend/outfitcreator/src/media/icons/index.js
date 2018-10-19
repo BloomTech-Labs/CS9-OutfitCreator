555958627252fa65b06b5698111bb89b30be0dee
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-10-19 15:40:11 -0700

    index.js - adds user-circle icon to index file

--- frontend/outfitcreator/src/media/icons/index.js
+++ frontend/outfitcreator/src/media/icons/index.js
@@ -1,39 +1,41 @@
 import bottom from './bottom.png';
 import casualShoes from './casualShoes.png';
 import dress from './dress.png';
+import facebook from './facebook.png';
 import formalShoes from './formalShoes.png';
+import github from './github.png';
+import google from './google.png';
 import jacket from './jacket.png';
 import leggings from './leggings.png';
+import locked from './locked.png';
 import pants from './pants.png';
+import refresh from './refresh-button.png';
 import shirt from './shirt.png';
 import shorts from './shorts.png';
 import skirt from './skirt.png';
 import sweater from './sweater.png';
 import top from './top.png';
-import locked from './locked.png';
 import unlocked from './unlocked.png';
-import refresh from './refresh-button.png';
-import google from './google.png';
-import facebook from './facebook.png';
-import github from './github.png';
+import userCircle from './user-circle.png';
 
 export const Icons = {
   bottom,
   casualShoes,
   dress, 
   formalShoes,
+  facebook,
+  github,
+  google,
   jacket,
   leggings,
+  locked,
   pants,
+  refresh,
   shirt,
   shorts,
   skirt,
   sweater,
   top,
-  locked,
   unlocked,
-  refresh,
-  google,
-  facebook,
-  github,
+  userCircle,
 }
