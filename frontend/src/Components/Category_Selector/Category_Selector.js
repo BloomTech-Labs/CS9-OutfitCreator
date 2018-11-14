3a3a3e6d16cf6cdbafa8a24761800a4594df30af
Devon Bacon <15711397+devbacon@users.noreply.github.com>
2018-11-14 12:54:33 -0800

    Category_Selector - sets up basic component structure and style sheet

---
+++ frontend/src/Components/Category_Selector/Category_Selector.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
+
+import { ROOT_URL } from '../../config';
+
+class Landing extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+
+		};
+	}
+
+	componentDidMount() {
+
+	}
+
+
+	render() {
+		return (
+			<div className="category-container">
+
+			</div>
+		);
+	}
+}
+
+export default Landing;
